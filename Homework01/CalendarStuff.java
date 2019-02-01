0f3037bba1a4a3814f8051c7266a0d44db12a074
Parker Bath <45578338+parkerbath@users.noreply.github.com>
2019-01-31 20:52:35 -0800

    Updated CalendarStuff.Java
    The only code that is missing is the daysBetween

--- Homework01/CalendarStuff.java
+++ Homework01/CalendarStuff.java
@@ -1,10 +1,10 @@
-/**
- *  File name     :  CalendarStuff.java
+
+ /*  File name     :  CalendarStuff.java
  *  Purpose       :  Provides a class with supporting methods for CountTheDays.java program
  *  Author        :  B.J. Johnson (prototype)
  *  Date          :  2017-01-02 (prototype)
- *  Author        :  <your name here>
- *  Date          :  <date of writing here>
+ *  Author        :  <Parker Bath>
+ *  Date          :  <1-31-19>
  *  Description   :  This file provides the supporting methods for the CountTheDays program which will
  *                   calculate the number of days between two dates.  It shows the use of modularization
  *                   when writing Java code, and how the Java compiler can "figure things out" on its
@@ -54,7 +54,7 @@ public class CalendarStuff {
    * An array containing the number of days in each month
    *  NOTE: this excludes leap years, so those will be handled as special cases
    */
-   private static int[]    days        = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+   private static int[]    days = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 
   /**
    * The constructor for the class
@@ -70,7 +70,14 @@ public class CalendarStuff {
    * @return         boolean which is true if the parameter is a leap year
    */
    public static boolean isLeapYear( long year ) {
-      return true;
+      if(year % 100 == 0){
+         return false;
+      }else if(year % 400 == 0){
+         return true;
+      }else if(year % 4 == 0){
+         return true;
+      }
+      return false;
    }
 
   /**
@@ -81,10 +88,13 @@ public class CalendarStuff {
    * notes: remember that the month variable is used as an indix, and so must
    *         be decremented to make the appropriate index value
    */
-   public static long daysInMonth( long month, long year ) {
-      return 33;
+   public static long daysInMonth( long month, long year ){
+     if(isLeapYear(year) && month == 2){
+         return 29;
+      }
+      return days[(int)month-1];
    }
-
+   
   /**
    * A method to determine if two dates are exactly equal
    * @param    month1 long    containing month number, starting with "1" for "January"
@@ -96,6 +106,13 @@ public class CalendarStuff {
    * @return          boolean which is true if the two dates are exactly the same
    */
    public static boolean dateEquals( long month1, long day1, long year1, long month2, long day2, long year2 ) {
+       if(year1 != year2){
+         return false;
+      } else if(day1 != day2){
+         return false;
+      } else if(month1 != month2){
+         return false;
+      }
       return true;
    }
 
@@ -110,7 +127,21 @@ public class CalendarStuff {
    * @return          int    -1/0/+1 if first date is less than/equal to/greater than second
    */
    public static int compareDate( long month1, long day1, long year1, long month2, long day2, long year2 ) {
+    if(dateEquals(month1, 1, year1, month2, 1, year2) == true){
       return 0;
+      }
+   if(year1 > year2){
+      return 1;
+      }
+   if(year1 == year2){
+      if(month1 > month2){
+         return 1;
+         } 
+       else {
+         return -1;
+         }
+      }
+      return -1;
    }
 
   /**
@@ -123,7 +154,10 @@ public class CalendarStuff {
    *         be decremented to make the appropriate index value
    */
    public static boolean isValidDate( long month, long day, long year ) {
-      return true;
+    if(month == 2 && isLeapYear(year) && day == 29){
+         return true;
+    }
+   return (month <= 12 && month >= 1) && (day >= 1 && day <= days[(int)month-1]) && (year >= 1 && year <= 9999);
    }
 
   /**
@@ -132,22 +166,62 @@ public class CalendarStuff {
    * @return         String containing the string value of the month (no spaces)
    */
    public static String toMonthString( int month ) {
-      switch( month - 1 ) {
+   String stringMonth;
+     switch (month) {
+         case 1:  stringMonth = "January";
+               break;
+         case 2:  stringMonth = "February";
+               break;
+         case 3:  stringMonth = "March";
+               break;
+         case 4:  stringMonth = "April";
+               break;
+         case 5:  stringMonth = "May";
+               break;
+         case 6:  stringMonth = "June";
+               break;
+         case 7:  stringMonth = "July";
+               break;
+         case 8:  stringMonth = "August";
+               break;
+         case 9:  stringMonth = "September";
+               break;
+         case 10: stringMonth = "October";
+               break;
+         case 11: stringMonth = "November";
+               break;
+         case 12: stringMonth = "December";
+               break;
          default: throw new IllegalArgumentException( "Illegal month value given to 'toMonthString()'." );
-      }
+        }
+        return stringMonth;
    }
-
   /**
    * A method to return a string version of the day of the week name
    * @param    day int    containing day number, starting with "1" for "Sunday"
    * @return       String containing the string value of the day (no spaces)
    */
    public static String toDayOfWeekString( int day ) {
-      switch( day - 1 ) {
-         default       : throw new IllegalArgumentException( "Illegal day value given to 'toDayOfWeekString()'." );
-      }
+  String stringDayOfWeek;
+     switch (day) {
+         case 1:  stringDayOfWeek = "Sunday";
+               break;
+         case 2:  stringDayOfWeek = "Monday";
+               break;
+         case 3:  stringDayOfWeek = "Tuesday";
+               break;
+         case 4:  stringDayOfWeek = "Wednesday";
+               break;
+         case 5:  stringDayOfWeek = "Thursday";
+               break;
+         case 6:  stringDayOfWeek = "Friday";
+               break;
+         case 7:  stringDayOfWeek = "Saturday";
+               break;
+       default: throw new IllegalArgumentException( "Illegal day value given to 'toDayOfWeekString()'." );
+        }
+        return stringDayOfWeek;
    }
-
   /**
    * A method to return a count of the total number of days between two valid dates
    * @param    month1 long   containing month number, starting with "1" for "January"
@@ -159,8 +233,26 @@ public class CalendarStuff {
    * @return          long   count of total number of days
    */
    public static long daysBetween( long month1, long day1, long year1, long month2, long day2, long year2 ) {
+int daysInBetween = 0;
+     if (year1 == year2) {
+        for (let i = 0; i < month1; i++){
+        daysInBetween += days [i];
+        }
+      return daysInBetween;
+     }
+
+
+
+
+
+
       long dayCount = 0;
       return dayCount;
    }
 
 }
+
+
+
+
+
